bc1d5cb9ee614b4bac2d617af77efa2e4c818547
NVManh <nvmanhopb@gmail.com>
2018-09-03 23:07:31 +0700

    create Main UI

--- app/src/main/java/com/framgia/nvmanh/boxmovie/screen/popular/PopularActivity.java
+++ app/src/main/java/com/framgia/nvmanh/boxmovie/screen/popular/PopularActivity.java
@@ -1,9 +1,18 @@
 package com.framgia.nvmanh.boxmovie.screen.popular;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
 import com.framgia.nvmanh.boxmovie.data.model.Movie;
 import com.framgia.nvmanh.boxmovie.screen.base.BaseGenresActivity;
 
 public class PopularActivity extends BaseGenresActivity {
+
+    public static Intent getPopularIntent(Context context){
+        Intent intent = new Intent(context, PopularActivity.class);
+        return intent;
+    }
 
     @Override
     public String getType() {
