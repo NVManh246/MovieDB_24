836ae373ec1a748da7e49e49953711f2d9021c64
NVManh <nvmanhopb@gmail.com>
2018-09-10 14:00:05 +0700

    display production company of movie

--- app/src/main/java/com/framgia/nvmanh/boxmovie/screen/overview/OverviewViewModel.java
+++ app/src/main/java/com/framgia/nvmanh/boxmovie/screen/overview/OverviewViewModel.java
@@ -2,12 +2,18 @@ package com.framgia.nvmanh.boxmovie.screen.overview;
 
 import android.content.Context;
 import android.databinding.ObservableField;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.framgia.nvmanh.boxmovie.data.model.MovieDetail;
+import com.framgia.nvmanh.boxmovie.screen.search.VerticalSpaceItemDecoration;
 
 public class OverviewViewModel {
+    private static final int SPACING = 10;
+
     public ObservableField<MovieDetail> observableMovie = new ObservableField<>();
     public ObservableField<GenresAdapter> observableGenresAdapter = new ObservableField<>();
+    public ObservableField<CompanyAdapter> observableCompanyAdapter = new ObservableField<>();
 
     private Context mContext;
 
@@ -18,5 +24,10 @@ public class OverviewViewModel {
     public void start(MovieDetail movieDetail){
         observableMovie.set(movieDetail);
         observableGenresAdapter.set(new GenresAdapter(mContext, movieDetail.getGenres()));
+        observableCompanyAdapter.set(new CompanyAdapter(movieDetail.getCompanies()));
+    }
+
+    public RecyclerView.ItemDecoration getItemDecoration(){
+        return new VerticalSpaceItemDecoration(SPACING);
     }
 }
